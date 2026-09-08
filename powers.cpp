739b5bcf2443991b483838ed64e9596964d9c058
Samar-AbdElaziz11 <rase7363@gmail.com>
2026-09-09 00:44:18 +0300

    Create powers.cpp

---
+++ powers.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	
+	
+	int number;
+	
+	cout << " enter number?" << endl;
+	cin >> number;
+	
+	int power2 = number * number;
+	int power3 = number * number * number;
+	int power4 = number * number * number * number;
+	
+	
+	cout << "power2 =" << power2 << endl;
+	cout << " power3 ="  << power3 << endl;
+	cout << "power4= " << power4 << endl;
+
+	
+	
+	
+	return 0;
