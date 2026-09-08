b98fe38ea783e9f2bb2d5f8c12c52792ec74fc31
Samar-AbdElaziz11 <rase7363@gmail.com>
2026-09-09 00:28:26 +0300

    Create circle_area.cpp

---
+++ circle_area.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	
+	float D;
+	
+	cout << " enter D" << endl;
+	cin >> D;
+	
+	 const float PI = 3.14159;
+	
+	 float Area = PI * D * D / 4;
+	
+	cout <<  Area << endl;
+	
+
+
+	
+	return 0;
+	
+}
