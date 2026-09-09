2430a36984fbd73189f7eb4d26ed3110dd5e1dd9
Samar-AbdElaziz11 <rase7363@gmail.com>
2026-09-09 21:29:46 +0300

    Create Boolean_test.cpp

---
+++ Boolean_test.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	
+	bool result1 = (12 >= 12);
+	bool result2 = ( 12 > 7);
+	bool result3 = ( 8 < 6);
+	bool result4 = ( 8 == 8);
+	bool result5 = ( 12 <= 12);
+	bool result6 = ( 7 == 5);
+	
+	cout << "result1= " << result1 << endl;
+	cout << "result2= " << result2 << endl;
+	cout << "result3= " << result3 << endl;
+	cout << "result4= " << result4 << endl;
+	cout << "result5= " << result5 << endl;
+	cout << "result6= " << result6 << endl;
+	
+	return 0;
+	
+}
