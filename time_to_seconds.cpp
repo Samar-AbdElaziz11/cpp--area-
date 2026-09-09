125786363c58e251780c0b225d0234728ee2e3a6
Samar-AbdElaziz11 <rase7363@gmail.com>
2026-09-10 02:12:49 +0300

    Create time_to_seconds.cpp

---
+++ time_to_seconds.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double days, hours, minutes, seconds;
+    
+    cout << "enter days? " << endl;
+    cin >> days;
+    
+    cout << "enter hours? " << endl;
+    cin >> hours;
+    
+    cout << "enter minutes? " << endl;
+    cin >> minutes;
+    
+    cout << "enter seconds? " << endl;
+    cin >> seconds;
+    
+    double totalSeconds = round(days * 24 * 60 * 60) + (hours * 60 * 60) + (minutes * 60) + seconds;
+    
+    cout << "Total Seconds = " << totalSeconds << endl;
+    
+    return 0;
+}
