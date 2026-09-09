52ed4146e79a37fe62626fd4b9675ca19c13a031
Samar-AbdElaziz11 <rase7363@gmail.com>
2026-09-09 20:48:39 +0300

    Create change_calculator.cpp

---
+++ change_calculator.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	
+	
+	int totalBill, cashpaid;
+	
+	cout << " enter totalBill?" <<endl;
+	cin >> totalBill;
+	
+	cout << " cashpaid?" << endl;
+	cin >> cashpaid;
+	
+	int remainder = cashpaid - totalBill;
+	
+	cout << " remainder = " << remainder << endl;
+
+	
+	
+	
+	return 0;
+	
+}
