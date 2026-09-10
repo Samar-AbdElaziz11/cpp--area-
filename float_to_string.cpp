42752ae9bc9ed3b0634291a41226b7987da30a6f
Samar-AbdElaziz11 <rase7363@gmail.com>
2026-09-11 00:28:29 +0300

    Create float_to_string.cpp

---
+++ float_to_string.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+
+{
+	
+	float num = 55.23;
+	
+	string st;
+	
+	st = to_string (num);
+	
+	cout <<" st = " << st << endl;
+
+
+	return 0;
+}
