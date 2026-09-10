3dbf1ea458622f8320eebc5977ffe8678399abc5
Samar-AbdElaziz11 <rase7363@gmail.com>
2026-09-11 00:24:36 +0300

    Create int_to_string.cpp

---
+++ int_to_string.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+
+{
+	
+	int num = 20;
+	
+	string st;
+	
+	st = to_string (num);
+	
+	cout <<" st = " << st << endl;
+
+
+	return 0;
+}
