604a5730dfcfd77ad23de59d289c90f7609428b9
Samar-AbdElaziz11 <rase7363@gmail.com>
2026-09-11 00:26:50 +0300

    Create double_to_string.cpp

---
+++ double_to_string.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+
+{
+	
+	double num = 33.5;
+	
+	string st;
+	
+	st = to_string (num);
+	
+	cout <<" st = " << st << endl;
+
+
+	return 0;
+}
