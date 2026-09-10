a4cbd258e410f0df9adf6316b385370dc8081c42
Samar-AbdElaziz11 <rase7363@gmail.com>
2026-09-11 00:30:31 +0300

    Create casting_float_to_int.cpp

---
+++ casting_float_to_int.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+
+{
+	
+	float num = 55.23;
+	
+	int num_int = (int)num;
+	
+	cout << " num_int =" << num_int << endl;
+
+
+	return 0;
+}
